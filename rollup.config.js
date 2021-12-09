97a95a3c1e30a210023125707f2eaefdda748d08
Erik Blankinship <erikb@mediamods.com>
2021-12-09 10:22:51 -0500

    fix for security error from github bot

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import autoprefixer from 'autoprefixer';
 import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
-// import pkg from './package.json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
-// import svgr from '@svgr/rollup';
 import url from '@rollup/plugin-url';
 
 import postcss from 'rollup-plugin-postcss';
@@ -14,12 +12,13 @@ import tailwindcss from 'tailwindcss';
 const defCompExport = {
   plugins: [
     replace({
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
+      'process.env.NODE_ENV': JSON.stringify( 'production' ),
+      preventAssignment: true
     }),
-    //this seems to work for our purposes...
-    nodeResolve({
-      extensions: [".jsx"]
-    }),
+    //this [no longer] seems to work for our purposes...
+    // nodeResolve({
+    //   extensions: [".jsx"]
+    // }),
     babel({
       "exclude": 'node_modules/**',
       "babelHelpers": "bundled",
@@ -66,75 +65,75 @@ export default [
     } }, defCompExport
   ),
 
-  // Object.assign( {
-  //   input: 'pages/api/IconButton-framer.js',
-  //   output: {
-  //     file: 'lib/IconButton.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/IconButton-framer.js',
+    output: {
+      file: 'lib/IconButton.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 
-  // Object.assign( {
-  //   input: 'pages/api/InfoCard-framer.js',
-  //   output: {
-  //     file: 'lib/InfoCard.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/InfoCard-framer.js',
+    output: {
+      file: 'lib/InfoCard.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 
-  // Object.assign( {
-  //   input: 'pages/api/Logo-framer.js',
-  //   output: {
-  //     file: 'lib/Logo.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/Logo-framer.js',
+    output: {
+      file: 'lib/Logo.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
   
-  // Object.assign( {
-  //   input: 'pages/api/Menu-framer.js',
-  //   output: {
-  //     file: 'lib/Menu.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/Menu-framer.js',
+    output: {
+      file: 'lib/Menu.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
   
-  // Object.assign( {
-  //   input: 'pages/api/Menubar-framer.js',
-  //   output: {
-  //     file: 'lib/Menubar.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/Menubar-framer.js',
+    output: {
+      file: 'lib/Menubar.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 
-  // Object.assign( {
-  //   input: 'pages/api/PillButton-framer.js',
-  //   output: {
-  //     file: 'lib/PillButton.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/PillButton-framer.js',
+    output: {
+      file: 'lib/PillButton.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 
-  // Object.assign( {
-  //   input: 'pages/api/RoundButton-framer.js',
-  //   output: {
-  //     file: 'lib/RoundButton.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/RoundButton-framer.js',
+    output: {
+      file: 'lib/RoundButton.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 
-  // Object.assign( {
-  //   input: 'pages/api/TextHeading-framer.js',
-  //   output: {
-  //     file: 'lib/TextHeading.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/TextHeading-framer.js',
+    output: {
+      file: 'lib/TextHeading.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 
-  // Object.assign( {
-  //   input: 'pages/api/TextParagraph-framer.js',
-  //   output: {
-  //     file: 'lib/TextParagraph.js',
-  //     format: 'esm'
-  //   } }, defCompExport
-  // ),
+  Object.assign( {
+    input: 'pages/api/TextParagraph-framer.js',
+    output: {
+      file: 'lib/TextParagraph.js',
+      format: 'esm'
+    } }, defCompExport
+  ),
 ];
